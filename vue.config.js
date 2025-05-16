17f1685bad019e665e38ef17bfc9cce691ce6153
Tom <domanh15032002@gmail.com>
2025-05-16 16:13:15 +0700

    firt commit

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,8 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
 });
+module.exports = {
+  devServer: {
+    port: 3000, // đổi 3000 thành cổng bạn muốn
+  },
+};
